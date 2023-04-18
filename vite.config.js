e4ceea3f56fbef47f4fc75a1d6b42e2327d9f21c
Lukast2008 <lukast2008@gmail.com>
2023-04-18 22:05:05 +0300

    deploy 13

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/vite-deploy/",
-  
+  resolve: {
+    alias: {
+      "@tweets": "/vite-deploy/tweets",
+    },
+  },
 });
