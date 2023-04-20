3f1533de4d35ea264666d58d87b458ea18a50fc0
Lukast2008 <lukast2008@gmail.com>
2023-04-20 20:53:02 +0300

    deploy number ?

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import alias from "vite-plugin-alias";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    alias({
+      "/tweets": "/src/pages/Tweets.jsx",
+    }),
+  ],
   base: "/vite-deploy/",
 });
