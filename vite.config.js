8a4ab3b537acdc200e42a4a4bcb2065af85741b7
Lukast2008 <lukast2008@gmail.com>
2023-04-20 14:06:57 +0300

    deploy 22

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react(), svgr()],
-  base: "/vite-deploy/",
-  // resolve: {
-  //   alias: {
-  //     "~": (__dirname, "../src"),
-  //   },
-  // },
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        tweet: resolve(root, "tweet", "index.html"),
+      },
+    },
+  },
 });
